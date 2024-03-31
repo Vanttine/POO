ec395c0336fbe201d6e0228312c3c440b804bd43
Luiz <fellipevantine12@gmail.com>
2024-03-31 20:01:50 -0300

    Class criada

---
+++ src/PessoaFisica.php
@@ -0,0 +1,12 @@
+<?php 
+
+class PessoaFisica
+{
+    // aqui ficarao as propriedades e metodos
+    public $nome = 'Vantine';
+}
+
+$pessoaFisica = new PessoaFisica();
+
+echo '<pre>';
+var_dump( value: $pessoaFisica);
